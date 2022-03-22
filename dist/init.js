bc68613cac11335501e7843f299981f33c3887af
cup113 <3529706495@qq.com>
2022-03-22 19:08:10 +0800

    2.1.2 (Alpha) | Change lib

--- dist/init.js
+++ dist/init.js
@@ -1,3 +1,3 @@
 /// <reference path="./footer.ts"/>
-var Version = "2.1.1 (Alpha)";
+var Version = "2.1.2 (Alpha)";
 generate_footer(Version, "https://github.com/cup113/calculating-exercise/");
--- src/init.ts
+++ src/init.ts
@@ -1,3 +1,3 @@
 /// <reference path="./footer.ts"/>
-const Version = "2.1.1 (Alpha)";
+const Version = "2.1.2 (Alpha)";
 generate_footer(Version, "https://github.com/cup113/calculating-exercise/");
