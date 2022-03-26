0bb50167213fb5b7a75743bb6d5c3e7f77d9c74f
cup113 <3529706495@qq.com>
2022-03-26 22:34:48 +0800

    2.1.4 (Beta)

--- src/init.ts
+++ src/init.ts
@@ -1,3 +1,3 @@
 /// <reference path="./footer.ts"/>
-const Version = "2.1.3 (Alpha)";
+const Version = "2.1.4 (Beta)";
 generate_footer(Version, "https://github.com/cup113/calculating-exercise/", true, 2022, 2021);
