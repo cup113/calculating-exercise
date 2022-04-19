5cca46f9bd2e898804214717a1922bbaf1d195c5
cup113 <3529706495@qq.com>
2022-04-19 17:10:50 +0800

    2.1.9 (Beta) | Improve anchor display

--- dist/init.js
+++ dist/init.js
@@ -1,3 +1,3 @@
 /// <reference path="./footer.ts"/>
-var Version = "2.1.8 (Beta)";
+var Version = "2.1.9 (Beta)";
 generate_footer(Version, "https://github.com/cup113/calculating-exercise/", true, 2022, 2021);
